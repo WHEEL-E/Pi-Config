e80ef9013f63c8ef76fc1ee43a91326882b283a5
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-03-15 00:24:15 +0200

    Add config parameters

---
+++ uart-test.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+import serial
+
+
+def main(action):
+    port = serial.Serial(
+        "/dev/ttyS0",
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=5,
+    )
+    port.write(action.encode())
+
+
+while True:
+    main("a")
+    sleep(1)
+    main("b")
+    sleep(1)
--- uart-testing.py
+++
@@ -1,5 +0,0 @@
-import serial
-
-port = serial.Serial("/dev/ttyS0", 115200)
-action = "a"
-port.write(action.encode())
