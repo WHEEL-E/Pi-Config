d0f59cf6615f60d15423cf5fbea279e6cf6d0d60
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-03-15 00:37:45 +0200

    Add write_timeout parameter

--- uart-test.py
+++ uart-test.py
@@ -1,21 +1,19 @@
-from time import sleep
-
 import serial
 
 
 def main(action):
     port = serial.Serial(
-        "/dev/ttyS0",
+        port="/dev/ttyS0",
         baudrate=115200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
+        write_timeout=1,
     )
     port.write(action.encode())
 
 
-while True:
-    main("a")
-    sleep(1)
-    main("b")
-    sleep(1)
+main("a")
+main("b")
+main("c")
+main("d")
