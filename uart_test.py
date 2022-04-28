3f39515901c1c631bbfe4d94496d96c7c3038f31
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-04-28 17:23:12 +0200

    Fix the delay problem

--- uart_test.py
+++ uart_test.py
@@ -3,19 +3,23 @@ For more information on the serial module, see: https://pythonhosted.org/pyseria
 """
 
 import serial
+import time
 
+port = serial.Serial(
+    port="/dev/ttyS0",
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    write_timeout=1,
+)
 
-def main(action):
-    port = serial.Serial(
-        port="/dev/ttyS0",
-        baudrate=115200,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        write_timeout=1,
-    )
-    port.write(action.encode())
+def A():
+    port.write(b"A")
 
+def B():
+    port.write(b"B")
 
-main("A")
-main("B")
+A()
+time.sleep(2)
+B()
